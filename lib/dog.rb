978095c52be4f21ca6c4bf1f2ac65077737979f4
Randy Steele <steelerx155@gmail.com>
2019-10-23 03:04:39 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,33 @@
+ class Dog
+  def name
+    @name
+  end
+
+def name=(name)
+    @name = name
+  end
+  
+  def breed
+    @breed
+  end
+  
+  def breed=(breed)
+    @breed = breed
+  end
+end
+
+# class Dog
+#   def initalize(name)
+#     @name = name
+# end
+# def name=(this_dogs_name) 
+# this_dogs_name = name
+# end
+
+# def name
+#   @name
+# end
+
+# Fido = Dog.new
+# Fido.name = "Fido"
+# end
