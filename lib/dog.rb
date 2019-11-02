4820219ad1e07ffc4e8054428e7a6d9c2b4ddf37
Randy Steele <steelerx155@gmail.com>
2019-11-02 18:34:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -15,19 +15,3 @@ def name=(name)
     @breed = breed
   end
 end
-
-# class Dog
-#   def initalize(name)
-#     @name = name
-# end
-# def name=(this_dogs_name) 
-# this_dogs_name = name
-# end
-
-# def name
-#   @name
-# end
-
-# Fido = Dog.new
-# Fido.name = "Fido"
-# end
